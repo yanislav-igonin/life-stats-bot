3e07a12ecde2c3646711ee15174558fc472252a3
Yanislav Igonin <yanislavdabest@gmail.com>
2024-09-08 23:54:26 +0300

    add migration for quality of sleep

---
+++ src/database/migrations/1725828830698-quality-of-sleep.ts
@@ -0,0 +1,35 @@
+import { type MigrationInterface, type QueryRunner } from 'typeorm';
+
+export class QualityOfSleep1725828830698 implements MigrationInterface {
+  name = 'QualityOfSleep1725828830698';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "sleeps" DROP COLUMN "type"`);
+    await queryRunner.query(`DROP TYPE "public"."sleepsTypeEnum"`);
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "isAllowed"`);
+    await queryRunner.query(`ALTER TABLE "sleeps" ADD "wakeUpAt" TIMESTAMP`);
+    await queryRunner.query(`ALTER TABLE "sleeps" ADD "goToBedAt" TIMESTAMP`);
+    await queryRunner.query(
+      `CREATE TYPE "public"."sleepsQualityEnum" AS ENUM('bad', 'meh', 'good')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "sleeps" ADD "quality" "public"."sleepsQualityEnum"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "sleeps" DROP COLUMN "quality"`);
+    await queryRunner.query(`DROP TYPE "public"."sleepsQualityEnum"`);
+    await queryRunner.query(`ALTER TABLE "sleeps" DROP COLUMN "goToBedAt"`);
+    await queryRunner.query(`ALTER TABLE "sleeps" DROP COLUMN "wakeUpAt"`);
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "isAllowed" boolean NOT NULL DEFAULT false`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."sleepsTypeEnum" AS ENUM('go_to_bed', 'wake_up')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "sleeps" ADD "type" "public"."sleepsTypeEnum" NOT NULL`,
+    );
+  }
+}
