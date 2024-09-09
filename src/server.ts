653f7ed49aa5642ee848de38cb46fd84a6263e6d
Yanislav Igonin <yanislavdabest@gmail.com>
2024-09-09 17:08:03 +0300

    take needed fields

--- src/server.ts
+++ src/server.ts
@@ -14,21 +14,36 @@ class SuccessResponse {
   }
 }
 
+class ErrorResponse {
+  body: unknown;
+
+  constructor(body: unknown) {
+    this.body = {
+      data: body,
+      ok: false,
+    };
+  }
+}
+
 app.get('/sleep', async (context) => {
   const token = context.req.header('Authorization')?.replace('Bearer ', '');
   if (!token) {
     context.status(401);
-    return context.json('Unauthorized');
+    return context.json(new ErrorResponse('Unauthorized'));
   }
 
   const user = await UserModel.findOneBy({ token });
   if (!user) {
     context.status(401);
-    return context.json('Unauthorized');
+    return context.json(new ErrorResponse('Unauthorized'));
   }
 
-  const sleeps = await SleepModel.findBy({ userId: user.id });
-  context.json(new SuccessResponse(sleeps));
+  const sleeps = await SleepModel.find({
+    order: { createdAt: 'ASC' },
+    select: ['id', 'goToBedAt', 'wakeUpAt', 'quality'],
+    where: { userId: user.id },
+  });
+  return context.json(new SuccessResponse(sleeps));
 });
 
 export { app };
