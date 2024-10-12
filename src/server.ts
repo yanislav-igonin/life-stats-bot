69f0c91d4f14737f01639905256904f7f4008a51
Yanislav Igonin <yanislavdabest@gmail.com>
2024-10-12 13:12:34 +0300

    Typings

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { SleepModel, UserModel } from "database/models";
+import type { MoodOfDay, SleepQuality } from "database/models";
 import { Hono } from "hono";
+import type { Context } from "hono";
 import { cors } from "hono/cors";
 import { Between } from "typeorm";
 
@@ -26,7 +28,7 @@ class ErrorResponse {
 	}
 }
 
-app.get("/auth", async (context) => {
+const auth = async (context: Context) => {
 	const token = context.req.header("Authorization")?.replace("Bearer ", "");
 	if (!token) {
 		context.status(401);
@@ -41,23 +43,20 @@ app.get("/auth", async (context) => {
 		return context.json(new ErrorResponse("Unauthorized"));
 	}
 
+	context.set("user", user);
+};
+
+app.get("/auth", async (context) => {
+	await auth(context);
+	// @ts-expect-error - user is set in auth
+	const user: UserModel = context.get("user");
 	return context.json(new SuccessResponse(user));
 });
 
 app.get("/sleep", async (context) => {
-	const token = context.req.header("Authorization")?.replace("Bearer ", "");
-	if (!token) {
-		context.status(401);
-		return context.json(new ErrorResponse("Unauthorized"));
-	}
-
-	const user = await UserModel.findOneBy({
-		token,
-	});
-	if (!user) {
-		context.status(401);
-		return context.json(new ErrorResponse("Unauthorized"));
-	}
+	await auth(context);
+	// @ts-expect-error - user is set in auth
+	const user: UserModel = context.get("user");
 
 	const { from, to } = context.req.query();
 	const sleeps = await SleepModel.find({
@@ -71,6 +70,41 @@ app.get("/sleep", async (context) => {
 		},
 	});
 	return context.json(new SuccessResponse(sleeps));
+});
+
+type SleepBody = {
+	id: number;
+	wakeUpAt: string;
+	goToBedAt: string;
+	moodOfDay: MoodOfDay;
+	quality: SleepQuality;
+};
+
+app.post("/sleep", async (context) => {
+	await auth(context);
+	// @ts-expect-error - user is set in auth
+	const user: UserModel = context.get("user");
+
+	const { id, wakeUpAt, goToBedAt, moodOfDay, quality } =
+		await context.req.json<SleepBody>();
+
+	const sleep = await SleepModel.findOneBy({
+		id,
+		userId: user.id,
+	});
+
+	if (!sleep) {
+		return context.json(new ErrorResponse("Sleep not found"));
+	}
+
+	sleep.wakeUpAt = new Date(wakeUpAt) ?? sleep.wakeUpAt;
+	sleep.goToBedAt = new Date(goToBedAt) ?? sleep.goToBedAt;
+	sleep.moodOfDay = moodOfDay ?? sleep.moodOfDay;
+	sleep.quality = quality ?? sleep.quality;
+
+	await sleep.save();
+
+	return context.json(new SuccessResponse(sleep));
 });
 
 export { app };
